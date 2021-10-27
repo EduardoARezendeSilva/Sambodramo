91364d2021d2c49aa3222528b8578a43757b741b
Eduardo Augusto Rezende Silva <80038803+EduardoARezendeSilva@users.noreply.github.com>
2021-10-27 09:53:34 -0300

    Add files via upload

---
+++ Sambodramo/Sambodramo/CODE/DTO/EscolasDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sambodramo.CODE.DTO
+{
+    class EscolasDTO
+    {
+        private int _id;
+        private string _nome;
+        private string _ano;
+        private string _proprietario;
+
+        public int id { get => _id; set => _id = value; }
+        public string nome { get => _nome; set => _nome = value; }
+        public string ano { get => _ano; set => _ano = value; }
+        public string proprietario { get => _proprietario; set => _proprietario = value; }
+    }
+}
---
+++ Sambodramo/Sambodramo/UI/Frm_Escolas.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Sambodramo
+{
+    partial class Frm_Escolas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtProprietario = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.MaskedTextBox();
+            this.lbl_Nome = new System.Windows.Forms.Label();
+            this.lblAno = new System.Windows.Forms.Label();
+            this.lblProprietario = new System.Windows.Forms.Label();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.grdEscolas = new System.Windows.Forms.DataGridView();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdEscolas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(27, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtProprietario
+            // 
+            this.txtProprietario.Location = new System.Drawing.Point(444, 38);
+            this.txtProprietario.Name = "txtProprietario";
+            this.txtProprietario.Size = new System.Drawing.Size(100, 20);
+            this.txtProprietario.TabIndex = 2;
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(226, 38);
+            this.txtAno.Mask = "0000";
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(100, 20);
+            this.txtAno.TabIndex = 1;
+            this.txtAno.ValidatingType = typeof(System.DateTime);
+            // 
+            // lbl_Nome
+            // 
+            this.lbl_Nome.AutoSize = true;
+            this.lbl_Nome.Location = new System.Drawing.Point(24, 22);
+            this.lbl_Nome.Name = "lbl_Nome";
+            this.lbl_Nome.Size = new System.Drawing.Size(84, 13);
+            this.lbl_Nome.TabIndex = 4;
+            this.lbl_Nome.Text = "Nome da escola";
+            // 
+            // lblAno
+            // 
+            this.lblAno.AutoSize = true;
+            this.lblAno.Location = new System.Drawing.Point(223, 19);
+            this.lblAno.Name = "lblAno";
+            this.lblAno.Size = new System.Drawing.Size(132, 13);
+            this.lblAno.TabIndex = 5;
+            this.lblAno.Text = "Ano da ultima participação";
+            // 
+            // lblProprietario
+            // 
+            this.lblProprietario.AutoSize = true;
+            this.lblProprietario.Location = new System.Drawing.Point(441, 19);
+            this.lblProprietario.Name = "lblProprietario";
+            this.lblProprietario.Size = new System.Drawing.Size(105, 13);
+            this.lblProprietario.TabIndex = 6;
+            this.lblProprietario.Text = "Nome do propreitario";
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(27, 110);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(100, 23);
+            this.btnInserir.TabIndex = 3;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(162, 110);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(100, 23);
+            this.btnAlterar.TabIndex = 4;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(302, 110);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 23);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // grdEscolas
+            // 
+            this.grdEscolas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdEscolas.Location = new System.Drawing.Point(27, 166);
+            this.grdEscolas.Name = "grdEscolas";
+            this.grdEscolas.Size = new System.Drawing.Size(519, 272);
+            this.grdEscolas.TabIndex = 10;
+            this.grdEscolas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdEscolas_CellClick);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(444, 110);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(100, 23);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Frm_Escolas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 462);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.grdEscolas);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.lblProprietario);
+            this.Controls.Add(this.lblAno);
+            this.Controls.Add(this.lbl_Nome);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtProprietario);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Frm_Escolas";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdEscolas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtProprietario;
+        private System.Windows.Forms.MaskedTextBox txtAno;
+        private System.Windows.Forms.Label lbl_Nome;
+        private System.Windows.Forms.Label lblAno;
+        private System.Windows.Forms.Label lblProprietario;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView grdEscolas;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
